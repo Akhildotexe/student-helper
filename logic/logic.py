a33f31eef2fa278399180959f5f9145d85eb8d1e
kausthubh-coder <kausthubh2007@gmail.com>
2022-11-05 12:17:11 -0400

    updated structure and indentation fix

--- logic/logic.py
+++ logic/logic.py
@@ -7,16 +7,23 @@ def getCode(module):
     url_input = module.split(" ")
     URL = f"https://raw.githubusercontent.com/kausthubh-coder/apcs-codehs/master/{url_input[0]}/{url_input[1]}/{url_input[2]}"
     page = requests.get(URL)
-
+    # soup = BeautifulSoup(page.content, 'html.parser')
+    # return soup.get_text()
+    page = requests.get(URL)
     return page.text
 
 def getUrl(module):
     url_input = module.split(" ")
     URL = f"https://raw.githubusercontent.com/kausthubh-coder/apcs-codehs/master/{url_input[0]}/{url_input[1]}/{url_input[2]}"
     return URL
-    
+
 def type(code):
+    time.sleep(1)
     for i in code.split(" "):
-        keyboard.write(i)
-        time.sleep(.5)
-        keyboard.press_and_release('space')
+        if i != "" and i != " ":
+            print(i)
+            keyboard.write(i)
+            keyboard.press_and_release('space')
+            time.sleep(.5)
+    # time.sleep(1)
+    # keyboard.write(code)
--- pages/codehs.py
+++ pages/codehs.py
@@ -1,18 +1,18 @@
 import flet
 from flet import Column, ElevatedButton, Text, TextField
-import logic
+import logic.logic as logic
 import webbrowser
 
 def main(page):
 
-    module = TextField(label="unit module codehs assignment")
+    module = TextField(label="number name")
     greetings = Column()
-    t = Text(value="type unit module codehs assignment. example: unit-3 3.3 battleships-move.java: ",size=30,weight="bold")
+    t = Text(value="type unit mod assignment  example: unit-3 3.2 discounts.java",size=30,weight="bold")
     # t2 = Text(value="type esc to start typing")
 
     def btn_click(e):
         webbrowser.open(logic.getUrl(module.value))
-    
+        
     def btn_click2(e):
         code = logic.getCode(module.value)
         logic.type(code)
