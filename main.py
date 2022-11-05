3fde69b6660c63b3adc3e1860e122f38e69175c4
kausthubh-coder <kausthubh2007@gmail.com>
2022-11-05 13:46:05 -0400

    updated structure and indentation fix

--- main.py
+++ main.py
@@ -1,210 +1,53 @@
-from copy import deepcopy
-import webbrowser
-
 import flet
-from flet import (
-    AppBar,
-    Column,
-    Row,
-    Container,
-    IconButton,
-    Icon,
-    NavigationRail,
-    NavigationRailDestination,
-    Page,
-    Text,
-    Card,
-    Divider,
-    PopupMenuButton,
-    PopupMenuItem,
-)
-from flet import colors, icons
+from flet import Column, ElevatedButton, Text, TextField
+import webbrowser
+import keyboard
+import time
+import requests 
+from bs4 import BeautifulSoup
 
+def getCode(module):
+    url_input = module.split(" ")
+    URL = f"https://raw.githubusercontent.com/kausthubh-coder/apcs-codehs/master/{url_input[0]}/{url_input[1]}/{url_input[2]}"
+    page = requests.get(URL)
+    return page.text
 
-class DesktopAppLayout(Row):
-    """A desktop app layout with a menu on the left."""
+def getUrl(module):
+    url_input = module.split(" ")
+    URL = f"https://raw.githubusercontent.com/kausthubh-coder/apcs-codehs/master/{url_input[0]}/{url_input[1]}/{url_input[2]}"
+    return URL
 
-    def __init__(
-        self,
-        title,
-        page,
-        pages,
-        *args,
-        window_size=(800, 600),
-        **kwargs,
-    ):
-        super().__init__(*args, **kwargs)
+def type(code):
+    time.sleep(1)
+    for i in code.split(" "):
+        if i != "" and i != " ":
+            print(i)
+            keyboard.write(i)
+            keyboard.press_and_release('space')
+            time.sleep(.5)
 
-        self.page = page
-        self.pages = pages
+def main(page):
 
-        self.expand = True
+    page.title = "student helper - by kausthbuh"
+    module = TextField(label="number name")
+    greetings = Column()
+    t = Text(value="type unit mod assignment  example: unit-3 3.2 discounts.java",size=30,weight="bold")
+    # t2 = Text(value="type esc to start typing")
 
-        self.navigation_items = [navigation_item for navigation_item, _ in pages]
-        self.navigation_rail = self.build_navigation_rail()
-        self.update_destinations()
-        self._menu_extended = True
-        self.navigation_rail.extended = True
+    def btn_click(e):
+        webbrowser.open(getUrl(module.value))
+        
+    def btn_click2(e):
+        code = getCode(module.value)
+        type(code)
 
-        self.menu_panel = Row(
-            controls=[
-                self.navigation_rail,
-            ],
-            spacing=0,
-            tight=True,
-        )
-
-        page_contents = [page_content for _, page_content in pages]
-        self.content_area = Column(page_contents, expand=True)
-
-        self._was_portrait = self.is_portrait()
-        self._panel_visible = self.is_landscape()
-
-        self.set_content()
-
-        self._change_displayed_page()
-
-        self.page.on_resize = self.handle_resize
-
-        self.page.appbar = self.create_appbar()
-
-        self.window_size = window_size
-        self.page.window_width, self.page.window_height = self.window_size
-
-        self.page.title = title
-
-    def select_page(self, page_number):
-        self.navigation_rail.selected_index = page_number
-        self._change_displayed_page()
-
-    def _navigation_change(self, e):
-        self._change_displayed_page()
-        self.page.update()
-
-    def _change_displayed_page(self):
-        page_number = self.navigation_rail.selected_index
-        for i, content_page in enumerate(self.content_area.controls):
-            # update selected page
-            content_page.visible = page_number == i
-
-    def build_navigation_rail(self):
-        return NavigationRail(
-            selected_index=0,
-            label_type="none",
-            on_change=self._navigation_change,
-            # bgcolor=colors.SURFACE_VARIANT,
-        )
-
-    def update_destinations(self):
-        self.navigation_rail.destinations = self.navigation_items
-        self.navigation_rail.label_type = "all"
-
-    def handle_resize(self, e):
-        pass
-
-    def set_content(self):
-        self.controls = [self.menu_panel, self.content_area]
-        self.update_destinations()
-        self.navigation_rail.extended = self._menu_extended
-        self.menu_panel.visible = self._panel_visible
-
-    def create_appbar(self) -> AppBar:
-        appbar = AppBar(
-            # leading=menu_button,
-            # leading_width=40,
-            # bgcolor=colors.SURFACE_VARIANT,
-            toolbar_height=48,
-            # elevation=8,
-        )
-
-        appbar.actions = [
-            Row(
-                [
-                    PopupMenuButton(
-                        icon=icons.HELP,
-                        items=[
-                            PopupMenuItem(
-                                icon=icons.CONTACT_SUPPORT,
-                                text="Ask a question",
-
-                            ),
-                            PopupMenuItem(
-                                icon=icons.BUG_REPORT,
-                                text="Report a bug",
-                            ),
-                        ],
-                    )
-                ]
-            )
-        ]
-        return appbar
-
-
-def create_page(title: str, body: str):
-    return Row(
-        controls=[
-            Column(
-                horizontal_alignment="stretch",
-                controls=[
-                    Card(content=Container(Text(title, weight="bold"), padding=8)),
-                    Text(body),
-                ],
-                expand=True,
-            ),
-        ],
-        expand=True,
+    page.add(
+        t,
+        module,
+        ElevatedButton("get code", on_click=btn_click),
+        ElevatedButton("type code", on_click=btn_click2),
+        greetings,
     )
 
+flet.app(target=main)
 
-def main(page: Page):
-
-    pages = [
-        (
-            NavigationRailDestination(
-                icon=icons.LANDSCAPE_OUTLINED,
-                selected_icon=icons.LANDSCAPE,
-                label="main",
-            ),
-            create_page(
-                "student helper",
-                "This is an example page. It is a simple desktop layout with a menu on the left.",
-            ),
-        ),
-        (
-            NavigationRailDestination(
-                icon=icons.PORTRAIT_OUTLINED,
-                selected_icon=icons.PORTRAIT,
-                label="Menu Item B",
-            ),
-            create_page(
-                "Example Page B",
-                "This is an example page. It is a simple desktop layout with a menu on the left.",
-            ),
-        ),
-        (
-            NavigationRailDestination(
-                icon=icons.INSERT_EMOTICON_OUTLINED,
-                selected_icon=icons.INSERT_EMOTICON,
-                label="Example Page C",
-            ),
-            create_page(
-                "Example Page C",
-                "This is an example page. It is a simple desktop layout with a menu on the left.",
-            ),
-        ),
-    ]
-
-    menu_layout = DesktopAppLayout(
-        page=page,
-        pages=pages,
-        title="Basic Desktop App Layout",
-        window_size=(1280, 720),
-    )
-
-    page.add(menu_layout)
-
-
-if __name__ == "__main__":
-    flet.app(
-        target=main,
-    )
